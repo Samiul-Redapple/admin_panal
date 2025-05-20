137564a244ea12b1c3044e76c4ced7bbebed657a
Samiul-Redapple <samiul@redappletech.com>
2025-05-20 19:28:24 +0530

    add sidebar

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
             }`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              Navbar
+              <Navbar/>
             </div>
           </div>
             <div>
