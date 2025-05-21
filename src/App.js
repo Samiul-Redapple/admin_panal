73a6a64941e5391241004cde62202e1755415fe7
Samiul-Redapple <samiul@redappletech.com>
2025-05-21 15:54:23 +0530

    complete Navbar and fixed soem bug

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import '../src/App.css';
+import { useStateContext } from './contexts/ContextProvider';
  
 
 const App = () => {
-  const activeMenu = true; // This should be replaced with your actual state management logic
+  // const activeMenu = true; // This should be replaced with your actual state management logic
+  const { activeMenu } = useStateContext();
   return (
     <div>
       <BrowserRouter>
